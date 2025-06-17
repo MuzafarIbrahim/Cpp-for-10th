ff676bb86c33f8f635a41283479812834bd91c23
Muzafar21SW055 <21sw055@students.muet.edu.pk>
2025-06-17 17:53:22 +0500

    dataTypes and their usage.

---
+++ dataTypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 23;  // only integer values
+    float b = 2.2; // only decimal numbers
+    double c = 25; // decimal as well as integer numbers.
+    double d = 25.5;
+    string name = "Muzafar Ibrahim"; // full letters and characeters, paragraphs, lines etc
+    char grade = 'A';                // only letters or characters
+
+    cout << "My age is " << age << endl;
+    cout << "My name is " << name << endl;
+    cout << "My grade is " << grade << endl;
+
+    return 0;
+}
+
+/* char grade = 'A';
+bool isPassed = true;
+
+cout<<"Grade "<<grade<<endl;
+cout<<"Passed: "<<isPassed<<endl;
+*/
