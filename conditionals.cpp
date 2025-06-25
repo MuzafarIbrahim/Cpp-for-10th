be7f54119d5b373f307e0b4c83030f74f860f385
Muzafar21SW055 <21sw055@students.muet.edu.pk>
2025-06-25 19:16:15 +0500

    if-else statement done.

---
+++ conditionals.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int temp;
+    // cout << "Enter room temperature: ";
+    // cin >> temp;
+
+    // if (temp <= 16)
+    // {
+    //     cout << "Room is too cold." << endl;
+    // }
+    // else
+    // {
+    //     cout << "Room is too hot." << endl;
+    // }
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if (age >= 18)
+    // {
+    //     cout << "You are eligible to drive." << endl;
+    // }
+    // else
+    // {
+    //     cout << "You are not eligible to drive." << endl;
+    // }
+
+    // Check If you are eligible to vote. minimum age to vote is 18.
+
+    int number;
+    cout << "Enter any number: ";
+    cin >> number;
+
+    if (number > 0)
+    {
+        cout << "Number is positive." << endl;
+    }
+    else
+    {
+
+        cout << "Number is negative." << endl;
+    }
+
+    return 0;
+}
